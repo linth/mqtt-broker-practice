5190839e05180a4d0b4058930ca35f7ef02ac8f9
linth <gerogelin@oringnet.com>
2024-01-04 14:37:29 +0800

    use logger.

--- clusters/index.js
+++ clusters/index.js
@@ -44,6 +44,21 @@ logger.debug("Debug message");
 logger.silly("Silly message");
 
 function startAedes () {
+  // mq?: any;
+  // id?: string;
+  // persistence?: any;
+  // concurrency?: number;
+  // heartbeatInterval?: number;
+  // connectTimeout?: number;
+  // queueLimit?: number;
+  // maxClientsIdLength?: number;
+  // preConnect?: PreConnectHandler;
+  // authenticate?: AuthenticateHandler;
+  // authorizePublish?: AuthorizePublishHandler;
+  // authorizeSubscribe?: AuthorizeSubscribeHandler;
+  // authorizeForward?: AuthorizeForwardHandler;
+  // published?: PublishedHandler;
+
   const port = 1883
 
   const aedes = Aedes({
@@ -55,38 +70,47 @@ function startAedes () {
   const server = createServer(aedes.handle)
 
   server.listen(port, '0.0.0.0', function () {
-    console.log('Aedes listening on port:', port)
+    // console.log('Aedes listening on port:', port)
+    logger.info('Aedes listening on port:', port)
     aedes.publish({ topic: 'aedes/hello', payload: "I'm broker " + aedes.id })
   })
 
   server.on('error', function (err) {
-    console.log('Server error', err)
+    // console.log('Server error', err)
+    logger.error('Server error', err)
     process.exit(1)
   })
 
   aedes.on('subscribe', function (subscriptions, client) {
-    console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
-            '\x1b[0m subscribed to topics: ' + subscriptions.map(s => s.topic).join('\n'), 'from broker', aedes.id)
+    // console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
+    //         '\x1b[0m subscribed to topics: ' + subscriptions.map(s => s.topic).join('\n'), 'from broker', aedes.id)
+    logger.debug('MQTT client \x1b[32m' + (client ? client.id : client) +
+      '\x1b[0m subscribed to topics: ' + subscriptions.map(s => s.topic).join('\n'), 'from broker', aedes.id)
   })
 
   aedes.on('unsubscribe', function (subscriptions, client) {
-    console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
-            '\x1b[0m unsubscribed to topics: ' + subscriptions.join('\n'), 'from broker', aedes.id)
+    // console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
+    //         '\x1b[0m unsubscribed to topics: ' + subscriptions.join('\n'), 'from broker', aedes.id)
+    logger.debug('MQTT client \x1b[32m' + (client ? client.id : client) +
+      '\x1b[0m unsubscribed to topics: ' + subscriptions.join('\n'), 'from broker', aedes.id)
   })
 
   // fired when a client connects
   aedes.on('client', function (client) {
-    console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+    // console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+    logger.info('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
   })
 
   // fired when a client disconnects
   aedes.on('clientDisconnect', function (client) {
-    console.log('Client Disconnected: \x1b[31m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+    // console.log('Client Disconnected: \x1b[31m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+    logger.info('Client Disconnected: \x1b[31m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
   })
 
   // fired when a message is published
   aedes.on('publish', async function (packet, client) {
-    console.log('Client \x1b[31m' + (client ? client.id : 'BROKER_' + aedes.id) + '\x1b[0m has published', packet.payload.toString(), 'on', packet.topic, 'to broker', aedes.id)
+    // console.log('Client \x1b[31m' + (client ? client.id : 'BROKER_' + aedes.id) + '\x1b[0m has published', packet.payload.toString(), 'on', packet.topic, 'to broker', aedes.id)
+    logger.debug('Client \x1b[31m' + (client ? client.id : 'BROKER_' + aedes.id) + '\x1b[0m has published', packet.payload.toString(), 'on', packet.topic, 'to broker', aedes.id)
   })
 }
 
