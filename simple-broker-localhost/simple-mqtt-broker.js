37bf741496145eebf121e80751dd326ccc54151b
linth <zhua0404@gmail.com>
2024-01-17 20:12:34 +0800

    feat: add aedes authentication.

--- simple-broker-localhost/simple-mqtt-broker.js
+++ simple-broker-localhost/simple-mqtt-broker.js
@@ -2,6 +2,16 @@ const aedes = require('aedes')()
 const server = require('net').createServer(aedes.handle)
 const port = 1883
 
+function authenticate(client, username, password, callback) {
+  if (username === 'george' && password.toString() === '123') {
+    callback(null, true);
+  } else {
+    callback(null, false);
+  }
+}
+
+aedes.authenticate = authenticate;
+
 server.listen(port, () => {
   console.log('server started and listening on port ', port)
 })
--- simple-broker-localhost/subscriber.js
+++ simple-broker-localhost/subscriber.js
@@ -1,10 +1,18 @@
 const mqtt = require('mqtt');
+const { error } = require('winston');
+const { cli } = require('winston/lib/winston/config');
 
-const client = mqtt.connect('mqtt://localhost:1883');
+// add authenticate.
+const client = mqtt.connect('mqtt://localhost:1883', {
+  username: 'george',
+  password: '123'
+});
 
 client.on('connect', () => {
+  // client connect to mqtt broker.
   console.log('Subscriber connected to MQTT broker');
   
+  // client subscribe topics.
   client.subscribe('myTopic', (err) => {
     if (!err) {
       console.log('Subscriber subscribed to myTopic');
@@ -12,6 +20,7 @@ client.on('connect', () => {
   });
 });
 
+// 當收到 message 時候觸發
 client.on('message', (topic, message) => {
   console.log(`Received message on topic '${topic}': ${message.toString()}`);
   client.end();
