792cb30985c22cc59292d50a2f0c2ba0804aefaa
linth <zhua0404@gmail.com>
2024-01-17 20:15:45 +0800

    feat: add authentication for publisher.

--- simple-broker-localhost/publisher.js
+++ simple-broker-localhost/publisher.js
@@ -1,6 +1,9 @@
 const mqtt = require('mqtt');
 
-const client = mqtt.connect('mqtt://localhost:1883');
+const client = mqtt.connect('mqtt://localhost:1883', {
+  username: 'george',
+  password: '123',
+});
 
 client.on('connect', () => {
   console.log('Publisher connected to MQTT broker');
